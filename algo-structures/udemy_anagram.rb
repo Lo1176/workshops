0e0e9ad61f1ee3d25abb1a5b51577ad657513658
Laurent Binder <laurentbinder@gmail.com>
2020-10-15 15:48:04 +0200

    anagram udemy done with Class method

--- algo-structures/udemy_anagram.rb
+++ algo-structures/udemy_anagram.rb
@@ -1,11 +1,21 @@
 
 def anagram(string1, string2)
-  # string1 == string2.reverse
-  reverse_string2 = []
-  string2.chars.each_with_index do |_letter, index|
-    reverse_string2 << string2[string2.size - 1 - index]
-  end
-  string1 == reverse_string2.join
+  string1.formated == string2.formated
 end
 
+# formated String Class Method
+class String
+  def formated
+    delete(' ').downcase.chars.sort
+  end
+end
+
+p anagram(' do G', '  Go d  ')
+
+p anagram('clint eAstwood', 'old west action')
+
+p anagram('pubLic relations', 'Crap Built On Lies')
+
 p anagram('dog', 'god')
+
+p anagram('non', 'oui')
